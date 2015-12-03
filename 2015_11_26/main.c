f92a2edc1eef975d727704a785aefad461744e89
Mateusz Przybyla <przybyla.mat@gmail.com>
2015-12-03 16:52:03 +0100

    Poprawki do programu z cwiczen 26.11.2015

--- 2015_11_26/main.c
+++ 2015_11_26/main.c
@@ -1,68 +1,69 @@
-#define F_CPU 16000000UL
+#define F_CPU 1000000UL // u mnie jest inne taktowanie zegara (1 MHz)
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+
 uint8_t cyfra[] = { // deklaracja tablicy mapujacej cyfry na segmenty wyswietlacza
-    ~0b00111111,
-    ~0b00000110,
-    ~0b01011011,
-    ~0b01001111,
-    ~0b01100110,
-    ~0b01101101,
-    ~0b01111101,
-    ~0b00000111,
-    ~0b01111111,
-    ~0b01101111
+	~0b00111111,
+	~0b00000110,
+	~0b01011011,
+	~0b01001111,
+	~0b01100110,
+	~0b01101101,
+	~0b01111101,
+	~0b00000111,
+	~0b01111111,
+	~0b01101111
 };
 
-int licznik = 0;
+int licznik = 0; // mierzy ms ktore uplynely
 int cyfry[4];
 
 ISR(TIMER0_COMP_vect) {
-	if (licznik == 10000)
+	licznik += 2; // na moim mikrokontrolerze jest inne taktowanie zegara, stad podbijam licznik o 2
+	if (licznik >= 10000)
 		licznik = 0;
-	int ktoraCyfra = 0;
-	int tmpLicznik = licznik;
-	while (ktoraCyfra < 4) { // konwersja licznika na 4 cyfry
-		int cyfra = tmpLicznik % 10;
-		tmpLicznik /= 10;
-		cyfry[ktoraCyfra] = cyfra;
-		ktoraCyfra++;
-	}
-	licznik+= 4;
-	PORTA = cyfra[cyfry[0]]; // 
-	PORTB = 0x07; // wlaczenie ostatniej cyfry (zgaszenie pozostalych)
-	_delay_ms(1); // to bylo w wersji gdzie licznik byl w funkcji main, na zajeciach nie zdazylem zaimplementowac timera, wiec nie wiem jak to zadziala z delayem w przerwaniu
-	PORTA = cyfra[cyfry[1]];
-	PORTB = 0x0b; // wlaczenie przedostatniej cyfry (zgaszenie pozostalych)
-	_delay_ms(1);
-	PORTA = cyfra[cyfry[2]];
-	PORTB = 0x0d; // wlaczenie drugiej cyfry (zgaszenie pozostalych)
-	_delay_ms(1);
-	PORTA = cyfra[cyfry[3]] | (1 << PB7); // na zajeciach nie zrobilem kropki miedzy pierwsza a druga cyfra, 
-							 // ale jesli mialbym ja robic to zrobilbym to tak (PB7 przy zalozeniu ze kropka jest podpieta do pinu PB7)
-	PORTB = 0x0e; // wlaczenie pierwszej cyfry (zgaszenie pozostalych) 
-	_delay_ms(1); 
 }
 
 int main(void)
 {
     DDRA = 0xff; // porty A ustawione na wyjscie
     DDRB = 0xff; // porty B ustawione na wyjscie
-    PORTA = 0x01; // poczatkowa cyfra (bez wiekszego znaczenia)
+    PORTA = cyfra[0]; // poczatkowa cyfra (bez wiekszego znaczenia)
     PORTB = 0x0f; // poczatkowe wybranie ktore cyfry swieca (zadna)
-    
 	
-	OCR0 = 250; // gdy timer dojdzie do 250 bedzie przerwanie (TIMER0_COMP, wywolane porownaniem aktualnego stanu licznika z wartoscia rejestru OCR0)
-	TIMSK |= (1 << OCIE0); // ustawienie przerwania poprzez porownanie licznika z OCR0
-	TCCR0 |= (1 << CS00 | 1 << CS01); // ustawienie preskalera na 64, dzieki temu czas sie "przeskaluje" i bedzie sie zliczal 64 razy wolniej
-	// wynikowa konfiguracja daje jedno przerwanie na 250 podbic timera, ktore sie zdarza co 64 cykl zegar, czyli przerwanie bedzie co 16 000 cykli
-	// daje to czestotliwosc przerwan 1000 Hz (co 1 ms)
-	sei(); // aktywowanie globalnego systemu przerwan
-    while(1)
+	OCR0 = 125; // ta wartosc zalezy od tej w linii 39 i u mnie tez jest inna niz na zajeciach
+	TIMSK |= (1 << OCIE0);
+	TCCR0 |= (1 << CS01); // u mnie poprawnie liczy przy preskalerze ustawionym na 8
+	sei();
+
+    while (1) 
     {
-        
+		int ktoraCyfra = 0;
+		int tmpLicznik = licznik;
+		while (ktoraCyfra < 4) { // konwersja licznika na 4 cyfry
+			int cyfra = tmpLicznik % 10;
+			tmpLicznik /= 10;
+			cyfry[ktoraCyfra] = cyfra;
+			ktoraCyfra++;
+		}
+		PORTA = cyfra[cyfry[0]]; //
+		PORTB = 0x01; // wlaczenie ostatniej cyfry (zgaszenie pozostalych)
+		_delay_ms(1);
+
+		PORTA = cyfra[cyfry[1]];
+		PORTB = 0x02; // wlaczenie przedostatniej cyfry (zgaszenie pozostalych)
+		_delay_ms(1);
+
+		PORTA = cyfra[cyfry[2]];
+		PORTB = 0x04; // wlaczenie drugiej cyfry (zgaszenie pozostalych)
+		_delay_ms(1);
+
+		PORTA = cyfra[cyfry[3]] ^ (1 << PA7); // ostatnia cyfre wyswietlamy razem z kropka (przy zalozeniu ze jest na pinie PA7)
+		PORTB = 0x08; // wlaczenie pierwszej cyfry (zgaszenie pozostalych)
+		_delay_ms(1);
     }
 }
+
